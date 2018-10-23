e5bbe60e37da2ac1669c004e74f77f507ddce414
vagrant <vagrant@vagrant.vm>
2018-10-23 03:22:38 +0000

    feat:fix rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :clientes
   get 'home/index'
-  get 'home/saludo'
+  get '/saludo', to: 'home#saludo'
   root :to =>'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
